9abf3b40ea807c4a50ecde112c8b94aa638322ab
Amit Prasad <17amitprasad@gmail.com>
2021-01-24 23:39:04 -0500

    Add comments, fix minor bugs

--- JSONTokenizer.h
+++ JSONTokenizer.h
@@ -27,14 +27,47 @@ typedef struct JSONToken {
     struct JSONToken* next;
     struct JSONToken* child;
 } JSONToken;
-
+/**
+ * Return a JSONToken that represents the root node of a JSON objects contained
+ * in char** str. The Token must be freed using freeJSON(token);
+ */
 struct JSONToken* parseJSON(char** str);
+/**
+ * Used by parseJSON to recursively tokenize JSON objects.
+ */
 int tokenize(char** str, int* position, struct JSONToken* token);
+/**
+ * Used to tokenize one value (e.g, string, number, boolean, etc), or if it is
+ * another array/object, call tokenize on it again.
+ */
 int tokenizeValue(struct JSONToken* token, char** str, int* position);
+/**
+ * Creates an empty JSONToken
+ */
 struct JSONToken* createEmptyToken();
+/**
+ * Print a JSON to the file descriptor in fp. This can be either stdout, or also
+ * can be used by something created by open_memstream, etc.
+ */
 void printJSON(struct JSONToken* object, char** str, FILE* fp);
+/**
+ * Recursive helper for printJSON.
+ */
 void printHelper(struct JSONToken* object, char** str, int indent, FILE* fp);
+/**
+ * Helper to print indentation for printJSON
+ */
 void printIndent(int indent, FILE* fp);
+/**
+ * Set a Formatted JSON string in char** str
+ */
 char* getFormattedString(struct JSONToken* object, char** str);
+/**
+ * Free a JSONToken recursively
+ */
 void freeJSON(struct JSONToken* object);
+/**
+ * Sets the error position in a global external variable called errorLoc. Used
+ * to identify error positions.
+ */
 void setErrorLoc(int loc);
--- ediSON.c
+++ ediSON.c
@@ -707,12 +707,12 @@ void editorSave() {
     char *buf = editorRowsToString(&len);
     int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
     if (fd != -1) {
-        if (ftruncate(fd, len) != -1) {
-            if (write(fd, buf, len) == len) {
+        if (ftruncate(fd, len-1) != -1) {
+            if (write(fd, buf, len-1) == len-1) {
                 close(fd);
                 free(buf);
                 E.dirty = 0;
-                editorSetStatusMessage("%d bytes written to disk", len);
+                editorSetStatusMessage("%d bytes written to disk", len-1);
                 return;
             }
         }
